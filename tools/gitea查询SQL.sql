257ecc680aea5ffc91110d76867bbc983d0b3a04
zhangyu <zhangyu@micromach.cn>
2017-11-23 16:11:24 +0800

    增加一些查询gitea数据库issue的sql

---
+++ tools/gitea查询SQL.sql
@@ -0,0 +1,76 @@
+# 查询所有issue及其里程碑和标签
+SELECT
+	i. NAME AS 'issue名称',
+	r.lower_name AS '仓库名',
+	m. NAME AS '里程碑',
+	l. NAME AS '标签'
+FROM
+	(
+		issue i
+		LEFT JOIN milestone m ON i.milestone_id = m.id
+		LEFT JOIN repository r ON i.repo_id = r.id
+		INNER JOIN issue_label il ON i.id = il.issue_id
+		INNER JOIN label l ON il.label_id = l.id
+	);
+
+# 查询所有issue及其里程碑和标签,并将标签合并(用逗号分隔)，使用里程碑排序
+SELECT
+	i. NAME AS 'issue名称',
+	r.lower_name AS '仓库名',
+	m. NAME AS '里程碑',
+	GROUP_CONCAT(l. NAME SEPARATOR ',') AS '标签'
+FROM
+	(
+		issue i
+		INNER JOIN milestone m ON i.milestone_id = m.id
+		LEFT JOIN repository r ON i.repo_id = r.id
+		LEFT JOIN issue_label il ON i.id = il.issue_id
+		LEFT JOIN label l ON il.label_id = l.id
+	)
+GROUP BY
+	i. NAME
+ORDER BY
+	m.`name`;
+
+# 选择标签是'类型/BUG'的issue
+SELECT
+	i. NAME AS 'issue名称',
+	r.lower_name AS '仓库名',
+	m. NAME AS '里程碑',
+	l. NAME AS '标签'
+FROM
+	(
+		issue i
+		LEFT JOIN milestone m ON i.milestone_id = m.id
+		LEFT JOIN repository r ON i.repo_id = r.id
+		INNER JOIN issue_label il ON i.id = il.issue_id
+		INNER JOIN label l ON il.label_id = l.id
+	)
+WHERE
+	(
+		r.lower_name = 'assert'
+		AND r.lower_name = 'assert'
+		AND l. NAME = '类型/BUG'
+	);
+
+# 选择标签是'类型/BUG'的issue，并且issue的标题包含'不合适'(%代表任意字符)
+SELECT
+	i. NAME AS 'issue名称',
+	r.lower_name AS '仓库名',
+	m. NAME AS '里程碑',
+	l. NAME AS '标签'
+FROM
+	(
+		issue i
+		LEFT JOIN milestone m ON i.milestone_id = m.id
+		LEFT JOIN repository r ON i.repo_id = r.id
+		INNER JOIN issue_label il ON i.id = il.issue_id
+		INNER JOIN label l ON il.label_id = l.id
+	)
+WHERE
+	(
+		l. NAME = '类型/BUG'
+		AND r.lower_name = 'assert'
+		AND i. NAME LIKE '%不合适%'
+	);
+
