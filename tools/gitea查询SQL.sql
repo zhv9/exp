7c2d945a5c658de0ca2453bf4b20001acab2f0ad
zhangyu <zhangyu@micromach.cn>
2017-11-23 16:58:29 +0800

    增加评论数量列

--- tools/gitea查询SQL.sql
+++ tools/gitea查询SQL.sql
@@ -1,11 +1,13 @@
-# 查询所有issue及其里程碑和标签
+# 查询所有issue及其里程碑和标签和其他相关信息
 SELECT
+	r.lower_name AS '仓库名',
 	i.`name` AS 'issue名称',
 	cu.`name` AS '创建人',
-	r.lower_name AS '仓库名',
 	m.`name` AS '里程碑',
+	au.`name` AS '指派人',
 	l.`name` AS '标签',
-	au.`name` AS '指派人'
+	i.is_closed AS '是否关闭',
+	i.num_comments AS '评论数量'
 FROM
 	(
 		issue i
@@ -19,17 +21,23 @@ FROM
 
 # 查询所有issue及其里程碑和标签,并将标签合并(用逗号分隔)，使用里程碑排序
 SELECT
-	i.`name` AS 'issue名称',
 	r.lower_name AS '仓库名',
+	i.`name` AS 'issue名称',
+	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
-	GROUP_CONCAT(l.`name` SEPARATOR ',') AS '标签'
+	au.`name` AS '指派人',
+	GROUP_CONCAT(l.`name` SEPARATOR ',') AS '标签',
+	i.is_closed AS '是否关闭',
+	i.num_comments AS '评论数量'
 FROM
 	(
 		issue i
-		INNER JOIN milestone m ON i.milestone_id = m.id
+		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
-		LEFT JOIN issue_label il ON i.id = il.issue_id
-		LEFT JOIN label l ON il.label_id = l.id
+		LEFT JOIN `user` cu ON i.poster_id = cu.id
+		LEFT JOIN `user` au ON i.assignee_id = au.id
+		INNER JOIN issue_label il ON i.id = il.issue_id
+		INNER JOIN label l ON il.label_id = l.id
 	)
 GROUP BY
 	i.`name`
@@ -38,36 +46,47 @@ ORDER BY
 
 # 选择标签是'类型/BUG'的issue
 SELECT
-	i.`name` AS 'issue名称',
 	r.lower_name AS '仓库名',
+	i.`name` AS 'issue名称',
+	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
-	l.`name` AS '标签'
+	au.`name` AS '指派人',
+	l.`name` AS '标签',
+	i.is_closed AS '是否关闭',
+	i.num_comments AS '评论数量'
 FROM
 	(
 		issue i
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
+		LEFT JOIN `user` cu ON i.poster_id = cu.id
+		LEFT JOIN `user` au ON i.assignee_id = au.id
 		INNER JOIN issue_label il ON i.id = il.issue_id
 		INNER JOIN label l ON il.label_id = l.id
 	)
 WHERE
 	(
 		r.lower_name = 'assert'
-		AND r.lower_name = 'assert'
 		AND l.`name` = '类型/BUG'
 	);
 
 # 选择标签是'类型/BUG'的issue，并且issue的标题包含'不合适'(%代表任意字符)
 SELECT
-	i.`name` AS 'issue名称',
 	r.lower_name AS '仓库名',
+	i.`name` AS 'issue名称',
+	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
-	l.`name` AS '标签'
+	au.`name` AS '指派人',
+	l.`name` AS '标签',
+	i.is_closed AS '是否关闭',
+	i.num_comments AS '评论数量'
 FROM
 	(
 		issue i
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
+		LEFT JOIN `user` cu ON i.poster_id = cu.id
+		LEFT JOIN `user` au ON i.assignee_id = au.id
 		INNER JOIN issue_label il ON i.id = il.issue_id
 		INNER JOIN label l ON il.label_id = l.id
 	)
