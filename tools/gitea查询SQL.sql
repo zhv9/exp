0740cdc53088b67a1999624d174f9343b5da4b7c
zhangyu <zhangyu@micromach.cn>
2017-12-16 17:58:48 +0800

    添加创建、更新和关闭时间

--- tools/gitea查询SQL.sql
+++ tools/gitea查询SQL.sql
@@ -1,16 +1,22 @@
 # 查询所有issue及其里程碑和标签和其他相关信息
 SELECT
+    i.
 	r.lower_name AS '仓库名',
+    i.`index` AS 'index',
 	i.`name` AS 'issue名称',
 	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
 	au.`name` AS '指派人',
 	l.`name` AS '标签',
 	i.is_closed AS '是否关闭',
-	i.num_comments AS '评论数量'
+	i.num_comments AS '评论数量',
+    FROM_UNIXTIME(i.created_unix) AS '创建时间',
+    FROM_UNIXTIME(i.updated_unix) AS '更新时间',
+    FROM_UNIXTIME(c.updated_unix) AS '关闭时间'
 FROM
 	(
 		issue i
+        LEFT JOIN (SELECT * FROM `comment` c WHERE type = 2 GROUP BY issue_id ORDER BY issue_id DESC) c ON c.issue_id = i.id
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
 		LEFT JOIN `user` cu ON i.poster_id = cu.id
@@ -22,16 +28,21 @@ FROM
 # 查询所有issue及其里程碑和标签,并将标签合并(用逗号分隔)，使用里程碑排序
 SELECT
 	r.lower_name AS '仓库名',
+    i.`index` AS 'index',
 	i.`name` AS 'issue名称',
 	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
 	au.`name` AS '指派人',
 	GROUP_CONCAT(l.`name` SEPARATOR ',') AS '标签',
 	i.is_closed AS '是否关闭',
-	i.num_comments AS '评论数量'
+	i.num_comments AS '评论数量',
+    FROM_UNIXTIME(i.created_unix) AS '创建时间',
+    FROM_UNIXTIME(i.updated_unix) AS '更新时间',
+    FROM_UNIXTIME(c.updated_unix) AS '关闭时间'
 FROM
 	(
 		issue i
+        LEFT JOIN (SELECT * FROM `comment` c WHERE type = 2 GROUP BY issue_id ORDER BY issue_id DESC) c ON c.issue_id = i.id
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
 		LEFT JOIN `user` cu ON i.poster_id = cu.id
@@ -47,16 +58,21 @@ ORDER BY
 # 选择标签是'类型/BUG'的issue
 SELECT
 	r.lower_name AS '仓库名',
+    i.`index` AS 'index',
 	i.`name` AS 'issue名称',
 	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
 	au.`name` AS '指派人',
 	l.`name` AS '标签',
 	i.is_closed AS '是否关闭',
-	i.num_comments AS '评论数量'
+	i.num_comments AS '评论数量',
+    FROM_UNIXTIME(i.created_unix) AS '创建时间',
+    FROM_UNIXTIME(i.updated_unix) AS '更新时间',
+    FROM_UNIXTIME(c.updated_unix) AS '关闭时间'
 FROM
 	(
 		issue i
+        LEFT JOIN (SELECT * FROM `comment` c WHERE type = 2 GROUP BY issue_id ORDER BY issue_id DESC) c ON c.issue_id = i.id
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
 		LEFT JOIN `user` cu ON i.poster_id = cu.id
@@ -73,16 +89,21 @@ WHERE
 # 选择标签是'类型/BUG'的issue，并且issue的标题包含'不合适'(%代表任意字符)
 SELECT
 	r.lower_name AS '仓库名',
+    i.`index` AS 'index',
 	i.`name` AS 'issue名称',
 	cu.`name` AS '创建人',
 	m.`name` AS '里程碑',
 	au.`name` AS '指派人',
 	l.`name` AS '标签',
 	i.is_closed AS '是否关闭',
-	i.num_comments AS '评论数量'
+	i.num_comments AS '评论数量',
+    FROM_UNIXTIME(i.created_unix) AS '创建时间',
+    FROM_UNIXTIME(i.updated_unix) AS '更新时间',
+    FROM_UNIXTIME(c.updated_unix) AS '关闭时间'
 FROM
 	(
 		issue i
+        LEFT JOIN (SELECT * FROM `comment` c WHERE type = 2 GROUP BY issue_id ORDER BY issue_id DESC) c ON c.issue_id = i.id
 		LEFT JOIN milestone m ON i.milestone_id = m.id
 		LEFT JOIN repository r ON i.repo_id = r.id
 		LEFT JOIN `user` cu ON i.poster_id = cu.id
