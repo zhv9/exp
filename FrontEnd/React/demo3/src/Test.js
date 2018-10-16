773057f4c75fd9061059f429826b910a55c5b1b2
RobinZhang <zh.v@live.cn>
2018-10-17 00:34:11 +0800

    使用 ajax 时应该使用componentWillMount 来更新数据

--- FrontEnd/React/demo3/src/Test.js
+++ FrontEnd/React/demo3/src/Test.js
@@ -4,22 +4,33 @@ export default class Test extends Component {
   constructor(props) {
     super()
     console.log('constructor')
+    this.state = {
+      time: new Date()
+    }
   }
 
   render() {
     console.log('render')
     return (
       <div style={{ border: "solid black 1px" }}>
-        Test
+        <p>Test</p>
         <button type='button' onClick={() => this.setState({})}>Set State 更新</button>
         <button type='button' onClick={() => this.forceUpdate()}>forceUpdate</button>
+        <p>{this.state.time.getSeconds()}</p>
       </div>
     )
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    })
   }
 
   // 加载
   componentWillMount() {
     console.log(this.props.name + '组件将要加载 componentWillMount')
+    this.timeId = setInterval(() => this.tick(), 1000)
   }
 
   componentDidMount() {
@@ -48,4 +59,8 @@ export default class Test extends Component {
     console.log(this.props.name + '组件已经更新完毕 componentDidUpdate')
   }
 
+  componentWillUnmount() {
+    console.log(this.props.name + '组件将要卸载 componentWillUnmount')
+    clearInterval(this.timeId)
+  }
 }
