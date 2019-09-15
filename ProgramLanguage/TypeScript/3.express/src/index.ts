00a2c536765fc0927b086d3a857e16d999bdcec4
RobinZhang <zh.v@live.cn>
2019-09-15 22:33:49 +0800

    add movie example with mvc design pattern

--- ProgramLanguage/TypeScript/3.express/src/index.ts
+++ ProgramLanguage/TypeScript/3.express/src/index.ts
@@ -1,92 +1,108 @@
 import "reflect-metadata";
-import { createConnection, getManager } from "typeorm";
+import {
+  createConnection,
+  getManager,
+  Connection,
+  getConnection
+} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
 import { User } from "./entity/User";
 import { WorkItem } from "./entity/WorkItem";
+import { getDbConnection } from "./db";
+import { movieRouter } from "./controller/movie_controller";
 
-createConnection().then(async connection => {
+function registerRouters(app, routes) {
+  // register express routes from defined application routes
+  routes.forEach(route => {
+    (app as any)[route.method](
+      route.route,
+      (req: Request, res: Response, next: Function) => {
+        const result = new (route.controller as any)()[route.action](
+          req,
+          res,
+          next
+        );
+        if (result instanceof Promise) {
+          result.then(result =>
+            result !== null && result !== undefined
+              ? res.send(result)
+              : undefined
+          );
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
+        }
+      }
+    );
+  });
+}
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
+async function addUserData(connection: Connection) {
+  // insert new users for test
+  await connection.manager.save(
+    connection.manager.create(User, {
+      firstName: "Timber",
+      lastName: "Saw",
+      age: 27
+    })
+  );
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+  await connection.manager.save(
+    connection.manager.create(User, {
+      firstName: "Phantom",
+      lastName: "Assassin",
+      age: 24
+    })
+  );
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
-    });
+  const initWorkItem = new WorkItem();
+  initWorkItem.text = "test";
+  console.log(initWorkItem);
 
-    // setup express app here
-    // ...
+  await connection.manager.save(
+    connection.manager.create(WorkItem, {
+      text: "test"
+    })
+  );
+}
 
-    // start express server
-    app.listen(3000);
+// createConnection()
+//   .then(async connection => {
+//     // create express app
+//     const app = express();
+//     app.use(bodyParser.json());
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
+//     // register express routes from defined application routes
+//     registerRouters(app, Routes);
+//     // setup express app here
+//     // ...
 
-    const initWorkItem = new WorkItem();
-    initWorkItem.text = 'test';
-    console.log(initWorkItem);
-    
-    await connection.manager.save(connection.manager.create(WorkItem, {
-        text: 'test'
-    }));
+//     // start express server
+//     app.listen(3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+//     // insert new users for test
+//     addUserData(connection);
 
-}).catch(error => console.log(error));
+//     console.log(
+//       "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+//     );
+//   })
+//   .catch(error => console.log(error));
 
+(async () => {
+  await getDbConnection();
+  const port = 3000;
+  const app = express();
+  app.use(bodyParser.json());
+  app.use("/api/v1/movies", movieRouter);
 
-// createConnection().then(async connection => {
-//   const app = express();
-//   const router = express.Router();
+  registerRouters(app, Routes);
 
-//   router.get("", async (req, res, next) => {
-//     const workItemRepository = getManager().getRepository(WorkItem);
-//     try {
-//       const workItem = workItemRepository.find({
-//         order: {
-//           createdAt: "DESC"
-//         }
-//       });
-//     } catch (error) {
-//       next(error);
-//     }
-//   });
-
-//   router.post("", async (req, res, next) => {
-//     const workItem = new WorkItem();
-//     workItem.text = req.body.text;
-//     const workItemRepository = getManager().getRepository(WorkItem);
-//     try {
-//       res.json(await workItemRepository.save(workItem));
-//     } catch (error) {
-//       next(error);
-//     }
-//   });
-
-//   app.use("/work-item", router);
-//   app.use(bodyParser.json());
-
-//   app.listen(3001);
-// });
+  app.listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
+  });
+  const conn = getConnection();
+  addUserData(conn);
+})();
