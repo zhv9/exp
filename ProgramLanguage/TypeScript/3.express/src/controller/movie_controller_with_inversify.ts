c687513f47c8d71033818a899e7852e9a70f6737
RobinZhang <zh.v@live.cn>
2019-09-15 23:21:42 +0800

    add origin movie controller

---
+++ ProgramLanguage/TypeScript/3.express/src/controller/movie_controller_with_inversify.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { getRepository } from "../repositories/movie_repository";
+import { Movie } from "../Entity/Movie";
+
+const movieRouter = Router();
+
+movieRouter.get("/", function(req, res) {
+  const movieRepository = getRepository();
+  movieRepository
+    .find()
+    .then(movies => {
+      res.json(movies);
+    })
+    .catch((e: Error) => {
+      res.status(500);
+      res.send(e.message);
+    });
+});
+
+movieRouter.get("/:year", function(req, res) {
+  const movieRepository = getRepository();
+  movieRepository
+    .find({ year: Number(req.params.year) })
+    .then(movies => {
+      res.json(movies);
+    })
+    .catch((e: Error) => {
+      res.status(500);
+      res.send(e.message);
+    });
+});
+
+movieRouter.post("/", async function(req, res) {
+  const movieRepository = getRepository();
+  const newMovie = req.body;
+
+  if (typeof newMovie.title !== "string" || typeof newMovie.year !== "number") {
+    res.status(400);
+    res.send(`Invalid Movie!`);
+  }
+  try {
+    const response = await movieRepository.save(newMovie);
+    res.send(response);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+});
+
+export { movieRouter };
